0631767dc0c50f7e527788ab5660cd62b8e54dc4
medison0001 <23665281+medison0001@users.noreply.github.com>
2025-09-02 15:32:06 -0500

    Tipos de datos primitivos y por referencia en Java

--- src/platzi/play/Main.java
+++ src/platzi/play/Main.java
@@ -3,29 +3,27 @@ package platzi.play;
 import platzi.play.contenido.Pelicula;
 import platzi.play.plataforma.Usuario;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("PLATZI PLAY 🍿");
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Cual es tu nombre?");
-        String nombre = scanner.nextLine();
         Pelicula pelicula = new Pelicula();
         pelicula.titulo = "El señor de los anillos";
-        pelicula.anioEstreno = 2001;
+        pelicula.fechaEstreno = LocalDate.of(2018,10, 15);
         pelicula.genero = "Fantasía";
         pelicula.calificar(4.7);
 
-        System.out.println("Hola " + nombre + ", esto es Platzi Play!");
         Usuario usuario = new Usuario();
-        usuario.nombre = nombre;
+        usuario.nombre = "Juan";
+        usuario.fechaRegistro = LocalDateTime.of(2025, 12, 24, 17, 15, 14);
 
-        System.out.println(nombre + " cuantos años tienes?");
-        int edad = scanner.nextInt();
+        System.out.println(usuario.fechaRegistro);
+
         usuario.ver(pelicula);
 
-        System.out.println(nombre + " puedes ver contenido +" + edad);
     }
 }
