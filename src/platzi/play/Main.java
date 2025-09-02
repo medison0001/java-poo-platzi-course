1c7d6f6fe7b04ac7e3adc2b64927c6a42ca703c8
medison0001 <23665281+medison0001@users.noreply.github.com>
2025-09-02 15:19:48 -0500

    Creación de clases y objetos en Java

--- src/platzi/play/Main.java
+++ src/platzi/play/Main.java
@@ -1,7 +1,31 @@
 package platzi.play;
 
+import platzi.play.contenido.Pelicula;
+import platzi.play.plataforma.Usuario;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo! 🚀");
+        System.out.println("PLATZI PLAY 🍿");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Cual es tu nombre?");
+        String nombre = scanner.nextLine();
+        Pelicula pelicula = new Pelicula();
+        pelicula.titulo = "El señor de los anillos";
+        pelicula.anioEstreno = 2001;
+        pelicula.genero = "Fantasía";
+        pelicula.calificar(4.7);
+
+        System.out.println("Hola " + nombre + ", esto es Platzi Play!");
+        Usuario usuario = new Usuario();
+        usuario.nombre = nombre;
+
+        System.out.println(nombre + " cuantos años tienes?");
+        int edad = scanner.nextInt();
+        usuario.ver(pelicula);
+
+        System.out.println(nombre + " puedes ver contenido +" + edad);
     }
 }
