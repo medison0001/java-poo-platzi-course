4053017b36bc52b8c6138863ead4c51d33cbd5d5
medison0001 <23665281+medison0001@users.noreply.github.com>
2025-09-03 10:13:33 -0500

    Implementación de menús interactivos con while y switch, búsqueda y eliminación de elementos en listas de Java

--- src/platzi/play/Main.java
+++ src/platzi/play/Main.java
@@ -2,33 +2,80 @@ package platzi.play;
 
 import platzi.play.contenido.Pelicula;
 import platzi.play.plataforma.Plataforma;
-import platzi.play.plataforma.Usuario;
 import platzi.play.util.ScannerUtils;
 
 public class Main {
     public static final String NOMBRE_PLATAFORMA = "PLATZI PLAY 🍿";
     public static final String VERSION = "1.0.0";
 
+    public static final int AGREGAR = 1;
+    public static final int MOSTRAR_TODO = 2;
+    public static final int BUSCAR_POR_TITULO = 3;
+    public static final int ELIMINAR = 4;
+    public static final int SALIR = 5;
+
     public static void main(String[] args) {
         Plataforma plataforma = new Plataforma(NOMBRE_PLATAFORMA);
         System.out.println(NOMBRE_PLATAFORMA + " v" + VERSION);
 
-        String nombre = ScannerUtils.capturarTexto("Nombre del contenido");
-        String genero = ScannerUtils.capturarTexto("Genero del contenido");
-        int duracion = ScannerUtils.capturarNumero("Duracion del contenido");
-        double calificacion = ScannerUtils.capturarDecimal("Calificacion del contenido");
+        cargarPeliculas(plataforma);
 
-        Pelicula pelicula = new Pelicula(nombre, duracion, genero, calificacion);
-        Pelicula pelicula2 = new Pelicula("F1 The Movie", 220, "Acción");
+        while (true) {
+            int opcionElegida = ScannerUtils.capturarNumero("""
+                    Ingrese una de las siguientes opciones:
+                    1. Agregar contenido
+                    2. Mostrar todo
+                    3. Buscar por titulo
+                    4. Eliminar
+                    5. Salir
+                    """);
 
-        plataforma.agregar(pelicula);
-        plataforma.agregar(pelicula2);
-        System.out.println("Numero de elementos en la plataforma " + plataforma.getContenido().size());
-        plataforma.eliminar(pelicula2);
+            switch (opcionElegida) {
+                case AGREGAR -> {
+                    String nombre = ScannerUtils.capturarTexto("Nombre del contenido");
+                    String genero = ScannerUtils.capturarTexto("Genero del contenido");
+                    int duracion = ScannerUtils.capturarNumero("Duracion del contenido");
+                    double calificacion = ScannerUtils.capturarDecimal("Calificacion del contenido");
 
-        plataforma.mostrarTitulos();
+                    plataforma.agregar(new Pelicula(nombre, duracion, genero, calificacion));
+                }
+                case MOSTRAR_TODO -> plataforma.mostrarTitulos();
+                case BUSCAR_POR_TITULO -> {
+                    String nombreBuscado = ScannerUtils.capturarTexto("Nombre del contenido a buscar");
+                    Pelicula contenido = plataforma.buscarPorTitulo(nombreBuscado);
 
-        Usuario usuario = new Usuario("Juan", "juan@platzi.com");
-        usuario.ver(pelicula);
+                    if (contenido != null) {
+                        System.out.println(contenido.obtenerFichaTecnica());
+                    } else {
+                        System.out.println(nombreBuscado + " no existe dentro de " + plataforma.getNombre());
+                    }
+                }
+                case ELIMINAR -> {
+                    String nombreAEliminar = ScannerUtils.capturarTexto("Nombre del contenido a eliminar");
+                    Pelicula contenido = plataforma.buscarPorTitulo(nombreAEliminar);
+
+                    if (contenido != null) {
+                        plataforma.eliminar(contenido);
+                        System.out.println(nombreAEliminar + " eliminado! ❌");
+                    } else {
+                        System.out.println(nombreAEliminar + " no existe dentro de " + plataforma.getNombre());
+                    }
+                }
+                case SALIR -> System.exit(0);
+            }
+        }
+    }
+
+    private static void cargarPeliculas(Plataforma plataforma) {
+        plataforma.agregar(new Pelicula("Shrek", 90, "Animada"));
+        plataforma.agregar(new Pelicula("Inception", 148, "Ciencia Ficción"));
+        plataforma.agregar(new Pelicula("Titanic", 195, "Drama", 4.6));
+        plataforma.agregar(new Pelicula("John Wick", 101, "Acción"));
+        plataforma.agregar(new Pelicula("El Conjuro", 112, "Terror", 3.0));
+        plataforma.agregar(new Pelicula("Coco", 105, "Animada", 4.7));
+        plataforma.agregar(new Pelicula("Interstellar", 169, "Ciencia Ficción", 5));
+        plataforma.agregar(new Pelicula("Joker", 122, "Drama"));
+        plataforma.agregar(new Pelicula("Toy Story", 81, "Animada", 4.5));
+        plataforma.agregar(new Pelicula("Avengers: Endgame", 181, "Acción", 3.9));
     }
 }
