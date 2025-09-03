c3878f576ca706959df62c56cd3620ade2b8d81a
medison0001 <23665281+medison0001@users.noreply.github.com>
2025-09-02 21:20:25 -0500

    Constructores en Java para inicializar objetos con datos

--- src/platzi/play/plataforma/Usuario.java
+++ src/platzi/play/plataforma/Usuario.java
@@ -1,12 +1,19 @@
 package platzi.play.plataforma;
 
 import platzi.play.contenido.Pelicula;
+
 import java.time.LocalDateTime;
 
 public class Usuario {
     public String nombre;
     public String email;
     public LocalDateTime fechaRegistro;
+
+    public Usuario(String nombre, String email) {
+        this.nombre = nombre;
+        this.email = email;
+        this.fechaRegistro = LocalDateTime.now();
+    }
 
     public void ver(Pelicula pelicula) {
         System.out.println(nombre + " está viendo...");
